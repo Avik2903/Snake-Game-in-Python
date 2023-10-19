2121461a9a83e52487f8ea268891c1c27569ca8a
Avik Sarkar <76903360+Avik2903@users.noreply.github.com>
2023-10-19 19:35:29 +0530

    Update snakegame.py

--- snakegame.py
+++ snakegame.py
@@ -29,8 +29,7 @@ def Your_score(score):
 def our_snake(snake_block, snake_list):
     for x in snake_list:
         pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
- 
- 
+
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
     dis.blit(mesg, [dis_width / 6, dis_height / 3])
